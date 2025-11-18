37ea6c4f65d9616edfb26f6073dd38b397811c24
Bader <Baderasiri16@gmail.com>
2025-11-18 10:21:56 +0300

    Home(finished)/more CSS

--- KAU-IMS/obj/Debug/net8.0/KAU-IMS.AssemblyInfo.cs
+++ KAU-IMS/obj/Debug/net8.0/KAU-IMS.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KAU-IMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e745a7d6f316b6e8b25d2b19d076396ac694127b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+828101561a89c2a079df594bc95cc7e99dd0985e")]
 [assembly: System.Reflection.AssemblyProductAttribute("KAU-IMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KAU-IMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
